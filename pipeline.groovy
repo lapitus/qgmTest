7724ca416183c1d53365133f813e81e7ae42e156
Alexey Gaponchuk <lapitusik@gmail.com>
2021-05-13 23:17:11 +0300

    test5

--- pipeline.groovy
+++ pipeline.groovy
@@ -22,9 +22,9 @@ pipeline {
                     qgManager.checkFlag("meta")
 
                     println(qgManager.getFlagStatus())
-                    def response = httpRequest 'http://localhost:8080/jenkins/api/json?pretty=true'
-                    println("Status: "+response.status)
-                    println("Content: "+response.content)
+                    def response = httpRequest 'https://ya.ru'
+                    println("Status: " + response.status)
+                    println("Content: " + response.content)
                 }
             }
         }
