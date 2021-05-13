ddef53b587a693198bb8668bb7e6b9ad1e01e84a
Alexey Gaponchuk <lapitusik@gmail.com>
2021-05-13 23:25:07 +0300

    test6

--- pipeline.groovy
+++ pipeline.groovy
@@ -1,32 +1,51 @@
 @Library('way4sharedlib')_
 pipeline {
     agent { node { label 'master' } }
+
     stages {
+
         stage('Example Build') {
             steps {
                 echo "hello!"
             }
         }
 
-        stage('make classes') {
-            steps {
-                script {
-                    echo 'Making classes2'
-                    def yw = new YamlWorker(this)
-                    yw.doStuff()
+
+        parallel {
+            stage('make classes') {
+
+                steps {
+                    script {
+                        echo 'Making classes2'
+                        def yw = new YamlWorker(this)
+                        yw.doStuff()
 
 
-                    def qgManager = new QgManager(this)
+                        def qgManager = new QgManager(this)
 
-                    qgManager.checkFlag("sast")
-                    qgManager.checkFlag("meta")
+                        qgManager.checkFlag("sast")
+                        qgManager.checkFlag("meta")
 
-                    println(qgManager.getFlagStatus())
-//                    def response = httpRequest 'https://ya.ru'
-//                    println("Status: " + response.status)
-//                    println("Content: " + response.content)
+                        println(qgManager.getFlagStatus())
+
+                    }
                 }
             }
+
+            stage('Parallel 1') {
+                steps {
+                    echo "hello!"
+                }
+            }
+
+            stage('Parallel 2') {
+                steps {
+                    echo "hello!"
+                }
+            }
+
         }
+
+
     }
 }
